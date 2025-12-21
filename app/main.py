07a59fb78adcf0e973991177f452822e9325d705
Ben Lavi <87126461+Benlavi@users.noreply.github.com>
2025-12-21 14:30:41 +0200

    modified changes to work with render

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
+import os
 from fastapi import FastAPI
 from app.database import init_db
 from app.routers import health
 from app.routers import users
 from app.routers import tickets
 from app.routers import comments
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
@@ -22,3 +25,15 @@ app.include_router(comments.router, prefix="/api")
 @app.get("/")
 def read_root():
     return {"message": "Fitness API is running!"}
+
+
+
+FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[FRONTEND_URL],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
